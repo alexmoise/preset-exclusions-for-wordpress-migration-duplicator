8204ef9736b0b28fdd291dfe0db9e48fef6122e2
Alex Moise <alex@moise.pro>
2020-09-26 14:34:30 +0300

    Create preset-exclusions-for-wordpress-migration-duplicator.php

---
+++ preset-exclusions-for-wordpress-migration-duplicator.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Preset exclusions for WordPress Migration & Duplicator
+ * Plugin URI: https://github.com/alexmoise/preset-exclusions-for-wordpress-migration-duplicator
+ * GitHub Plugin URI: https://github.com/alexmoise/preset-exclusions-for-wordpress-migration-duplicator
+ * Description: A quick collection of paths and file types that needs to be excluded from site exports created with WordPress Migration & Duplicator by Webtofee
+ * Version: 1.0.0
+ * Author: Alex Moise
+ * Author URI: https://moise.pro
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {	exit(0);}
+
+
+
+
+
+
+?>
